f656f9a0e3a506409b76e576f73b3ce5b77324d7
Bastian <bastian.berndsen@gmail.com>
2025-12-04 16:54:12 -0800

    adding hyperlinks

--- script.js
+++ script.js
@@ -12,24 +12,30 @@ function interpolateColor(color1, color2, factor) {
 const light = [204, 204, 204]; // #ccc
 const dark = [34, 34, 34];     // #222
 
-for(let i=0; i<gridSize; i++) {
-  for(let j=0; j<gridSize; j++) {
+for (let i = 0; i < gridSize; i++) {
+  for (let j = 0; j < gridSize; j++) {
     const idx = i * gridSize + j;
     const cell = document.createElement('div');
     cell.className = 'cell';
-    const circle = document.createElement('div');
-    circle.className = 'circle';
 
-    if(idx === redIndex) {
-      circle.style.background = 'red';
+    // If this is the red dot, use <a>
+    if (idx === redIndex) {
+      const circleLink = document.createElement('a');
+      circleLink.className = 'circle';
+      circleLink.href = 'https://github.com/berni-b/';
+      circleLink.taget = '_blank';  
+      circleLink.style.background = 'red';
+      cell.appendChild(circleLink);
     } else {
+      const circle = document.createElement('div');
+      circle.className = 'circle';
       // Fade: diagonal from top-left to bottom-right
       const fade = (i + j) / (2 * (gridSize - 1));
       const rgb = interpolateColor(light, dark, fade);
       circle.style.background = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+      cell.appendChild(circle);
     }
 
-    cell.appendChild(circle);
     grid.appendChild(cell);
   }
 }
